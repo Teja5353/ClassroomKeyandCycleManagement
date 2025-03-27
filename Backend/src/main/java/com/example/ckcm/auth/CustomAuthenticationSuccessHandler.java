9d90df7585504a0f42935b059b6729852637b32e
Navaneeth <navaneethkhatravath048@gmail.com>
2025-03-27 11:21:33 +0530

    Exception handling

--- Backend/src/main/java/com/example/ckcm/auth/CustomAuthenticationSuccessHandler.java
+++ Backend/src/main/java/com/example/ckcm/auth/CustomAuthenticationSuccessHandler.java
@@ -1,4 +1,18 @@
 package com.example.ckcm.auth;
 
-public class CustomAuthenticationSuccessHandler {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import java.io.IOException;
+
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) 
+            throws IOException, ServletException {
+        // Redirecting to the dashboard after successful login
+        response.sendRedirect("/dashboard");
+    }
 }
